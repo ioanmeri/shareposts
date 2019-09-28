af594d012fc14df2ddb7287a8536346097efe198
im <im@example.com>
2019-09-28 16:32:19 +0300

    Add Pages, Bootstrap & Navbar

--- app/controllers/Pages.php
+++ app/controllers/Pages.php
@@ -6,7 +6,8 @@
 
 		public function index(){
 			$data = [
-				'title' => 'SharePosts'
+				'title' => 'SharePosts',
+				'description' => 'Simple social network build on the Basic MVC PHP framework'
 			];
 
 			$this->view('pages/index', $data);
@@ -14,7 +15,8 @@
 
 		public function about(){
 			$data = [
-				'title' => 'About Us'
+				'title' => 'About Us',
+				'description' => 'App to share posts with other users'
 			];
 
 			$this->view('pages/about', $data);
