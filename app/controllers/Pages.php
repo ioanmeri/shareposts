f2bfed4bfdc30ae024fe355758e27a3a8e4235bc
Ioannis Merianos <ioannismeri@gmail.com>
2019-10-02 11:46:32 +0300

    Add Post Form

--- app/controllers/Pages.php
+++ app/controllers/Pages.php
@@ -5,6 +5,10 @@
 		}
 
 		public function index(){
+			if(isLoggedIn()){
+				redirect('posts');
+			}
+
 			$data = [
 				'title' => 'SharePosts',
 				'description' => 'Simple social network build on the Basic MVC PHP framework'
