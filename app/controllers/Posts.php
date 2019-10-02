0f894a9878ded88ed7773f76c697d33002a0fb8d
Ioannis Merianos <ioannismeri@gmail.com>
2019-10-02 14:34:24 +0300

    Editing Posts

--- app/controllers/Posts.php
+++ app/controllers/Posts.php
@@ -67,6 +67,69 @@
 
 		}
 
+
+		public function edit($id){
+			// Get existing post from model
+			$post = $this->postModel->getPostById($id);
+
+			if($_SERVER['REQUEST_METHOD'] == 'POST'){
+				// // Authorize user
+				if($_SESSION['user_id'] != $post->user_id){
+					die('Hack attempt');
+				}
+
+				// Sanitize POST array
+				$_POST = filter_input_array(INPUT_POST, FILTER_SANITIZE_STRING);
+
+				$data = [
+					'id' => $id,
+					'title' => trim($_POST['title']),
+					'body' => trim($_POST['body']),
+					'user_id' => $_SESSION['user_id'],
+					'title_err' => '',
+					'body_err' => ''
+				];
+
+				// Validate data
+				if(empty($data['title'])){
+					$data['title_err'] = 'Please enter title';
+				}
+
+				if(empty($data['body'])){
+					$data['body_err'] = 'Please enter body text';
+				}
+
+				// Make sure no errors
+				if(empty($data['title_err']) && empty($data['body_err'])){
+					//Validated
+					if($this->postModel->updatePost($data)){
+						flash('post_message', 'Post Updated');
+						redirect('posts');
+					}else {
+						die('Something went wrong');
+					}
+				}else {
+					// Load the view with errors
+					$this->view('posts/edit', $data);
+				}
+
+			}else{
+				// Check for owner
+				if($post->user_id != $_SESSION['user_id']){
+					redirect('posts');
+				}
+
+				$data = [
+					'id' => $id,
+					'title' => $post->title,
+					'body' => $post->body
+				];
+
+				$this->view('posts/edit', $data);
+			}
+
+		}
+
 		public function show($id){
 			$post = $this->postModel->getPostById($id);
 			$user = $this->userModel->getUserById($post->user_id);
